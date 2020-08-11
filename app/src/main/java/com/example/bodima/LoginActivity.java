869ac0431f88d60ac32a46dc7a084014d8cab1d7
LAPTOP-BEASRH4L\ASUS <kalpanagaweshith@gmail.com>
2020-08-11 18:40:08 +0530

    Registration Completed

--- app/src/main/java/com/example/bodima/LoginActivity.java
+++ app/src/main/java/com/example/bodima/LoginActivity.java
@@ -2,13 +2,31 @@ package com.example.bodima;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.annotation.IncompleteAnnotationException;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textView=findViewById(R.id.signup);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(LoginActivity.this,Registration.class);
+                startActivity(intent);
+
+                Toast.makeText(LoginActivity.this,"Create an Account",Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
